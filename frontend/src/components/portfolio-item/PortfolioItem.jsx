49df50088a846e74312689b0d0b8d3f431ec9677
stijnheynderickx <stijn.heynderickx@elision.eu>
2019-05-02 12:53:04 +0200

    about pagina af

--- frontend/src/components/portfolio-item/PortfolioItem.jsx
+++ frontend/src/components/portfolio-item/PortfolioItem.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import scrollmagic from 'scrollmagic';
 import dicomenuBack from './../../images/frontLAYER.png';
 import dicomenuFRONT from './../../images/back_layer.png';
 import waterschenken from './../../images/Waterschenken-min.jpg';
@@ -9,10 +10,58 @@ import koppel from './../../images/Koppelres-min.jpg';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import CallToAction from '../../components/CallToAction/CallToAction';
-
+import {Controller, Scene} from 'react-scrollmagic';
+import {Tween, Timeline} from 'react-gsap';
+// https://scrollmagic.io/
+// https://github.com/bitworking/react-scrollmagic/tree/master/example/src/components/ScrollMagicExamples
+// https://bitworking.github.io/react-gsap/
 AOS.init();
 
 class PortfolioItem extends Component {
+
+    constructor(props) {
+        super(props);
+        this.controller = new scrollmagic.Controller({
+            globalSceneOptions: {
+                triggerHook: "onEnter",
+                duration   : "200%"
+            }
+        });
+        //  this.handleClick = this.handleClick.bind(this);
+    }
+
+    componentDidMount() {
+        // init controller
+
+        // create a scene
+        new scrollmagic.Scene({triggerElement: "#paralax"})
+                .setTween("#innerParalax", {
+                    y   : "80%",
+                    ease: "Linear.easeNone"
+                })
+                .addIndicators()
+                .addTo(this.controller);
+    }
+
+    handleClick() {
+        let container = document.getElementById('root');
+        let windowHeight = window.innerHeight;
+        let windowWidth = window.innerWidth;
+        let scrollArea = 1000 - windowHeight;
+        let image1 = document.getElementsByClassName('front');
+        let image2 = document.getElementsByClassName('back');
+
+        // update position of image 1 and image 2 when scroll event fires.
+        window.addEventListener('scroll', function() {
+            var scrollTop = window.pageYOffset || window.scrollTop;
+            var scrollPercent = scrollTop / scrollArea || 0;
+
+            image1.style.left = scrollPercent * window.innerWidth + 'px';
+            image2.style.left = 800 - scrollPercent * window.innerWidth * 0.6 + 'px';
+        });
+
+    }
+
     render() {
         return (
                 <div>
@@ -22,8 +71,8 @@ class PortfolioItem extends Component {
                             <h3>Vertalingen & verkopen van culinaire woordenboeken </h3>
                         </div>
                         <div className="image-layers">
-                            <Image src={dicomenuFRONT} alt="dicomenu" className="img"/>
-                            <Image src={dicomenuBack} alt="dicomenu" className="img"/>
+                            <Image src={dicomenuFRONT} alt="dicomenu" id="front" className="img front"/>
+                            <Image src={dicomenuBack} alt="dicomenu" id="back" className="img back"/>
                         </div>
                     </div>
                     <Grid className="portfolio_item-grid">
@@ -31,7 +80,7 @@ class PortfolioItem extends Component {
                             <Col className="dicomenu-website" data-aos="fade-up" data-aos-mirror="true">
                                 <p>Dicomenu kwam naar ons met de vraag of we heel hun oude website inclusief webshop in een nieuw jasje konden steken.</p>
                                 <p>Niet alleen wilden ze een vernieuwde huisstijl voor hun webshop, ze wensten ook graag dat hun klanten betalingen konden doen via PayPal.</p>
-                                <span className="no-problemo">Geen probleem voor ons!</span>
+                                <br/>
                                 <p>Bovendien verstuurde ze op regelmatige basis e-mails naar al hun klanten en wensten ze deze gepersonaliseerd met de huisstijl van de website. </p>
                                 <p>Ook dit hebben we tot een goed einde kunnen brengen, en dat zelfs in 2 talen !</p>
                             </Col>
@@ -42,22 +91,41 @@ class PortfolioItem extends Component {
                             </Col>
                         </Row>
                         <Row>
-                            <Col md={4} sm={12}>
-                                <div data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="1500"><Image src={ober} alt="dicomenu" rounded responsive className="img"/></div>
-                            </Col>
-                            <Col md={4} sm={12}>
-                                <div data-aos="fade-up" data-aos-easing="ease-in-out"
-                                     data-aos-duration="1500"><Image src={koppel} alt="dicomenu" rounded responsive className="img"/></div>
-                            </Col>
-                            <Col md={4} sm={12}>
-                                <div data-aos="fade-up" data-aos-easing="ease-in-out" data-aos-duration="1500"><Image src={waterschenken} alt="dicomenu" rounded responsive className="img"/></div>
-                            </Col>
+                            {/*<Col className="banner" id="paralax" sm={12}>*/}
+                            {/*<Image src={ober} alt="dicomenu" id="innerParalax" rounded responsive className="img innerBanner"/>*/}
+                            {/*</Col>*/}
+                            <Controller>
+                                <div className="section"/>
+                                <Scene
+                                        indicators={false}
+                                        duration="200%"
+                                        triggerHook="onEnter"
+                                >
+                                    <Timeline
+                                            wrapper={<div className="parallax"/>}
+                                    >
+                                        <Tween
+                                                position="0"
+                                                from={{
+                                                    yPercent: -50,
+                                                }}
+                                                to={{
+                                                    yPercent: 0,
+                                                }}
+                                        >
+                                            <img src={ober} alt=""/>
+                                        </Tween>
+                                    </Timeline>
+                                </Scene>
+                                <div className="section"/>
+                            </Controller>
+
                         </Row>
                         <Row>
                             <Col className="webhosting" data-aos="zoom-in-up" data-aos-delay="550">
                                 <p>Enkele maanden na de release contaceerde ze ons opnieuw omdat ze toch vonden dat de laadtijd van hun website gehost
                                     op een goedkope buitenlandse hosting niet zo snel was...</p>
-                                <p>We hebben de volledige website vervolgens overgezet naar onze webhosting server en dit resulteerde in een laadtijd van bijna <strong>3,5 seconde</strong> sneller !!!
+                                <p>We hebben de volledige website vervolgens overgezet naar onze webhosting server en dit resulteerde in een laadtijd van bijna <strong>7 seconden</strong> sneller !!!
                                 </p>
                             </Col>
                         </Row>
--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,25 +1,39 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {Jumbotron, Grid, Row, Col, Image, Button} from 'react-bootstrap';
+import mellowBazen from './../../images/jonasstijn.jpg';
+import CallToAction from '../../components/CallToAction/CallToAction';
 
 export default class Home extends Component {
     render() {
         return (
                 <Grid>
-                    <h2> About page</h2>
-                    <Col xs={12} sm={6} className="person">
-                        <Image src="assets/img/logo.png" rounded className="profile-pic"/>
-                        <h3> Stijn </h3>
-                    </Col>
-                    <Col xs={12} sm={6} className="person">
-                        <Image src="assets/img/logo.png" rounded className="profile-pic"/>
-                        <h3> Jonas </h3>
-                    </Col>
+                    <div className="about">
+                        <h1> Wie zijn wij</h1>
+
                     <Col xs={12} sm={12} className="person">
-                        <h2>Stijn & Jonas </h2>
-                        <p>Hebben dezelfde opleiding gedaan op Kdg nalmelijk Multimedia & Web UX</p>
+                        <article className="post">
+                            <div>
+                                <Image src={mellowBazen} className="stijnJonas"/>
+                            </div>
+                            <div className="post__container">
+                                <span className="post__category">Wat meer over wie er achter mellow zit</span>
+                                <div className="post__content">
+                                    <header>
+                                        <time className="post__time">September 28 2017</time>
+                                        <h2 className="post__header"><span>Hoe het</span> <span>allemaal</span> <span>begon</span></h2>
+                                    </header>
+                                    <p className="post__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean a augue justo. In mollis erat in elit tempus, feugiat luctus ex
+                                        sollicitudin. Maecenas euismod tortor dolor, vel blandit augue aliquam sit amet. Vestibulum et eros mollis, laoreet nisi ac, condimentum sapien. Aliquam nec
+                                        nunc enim.</p>
+                                </div>
+                            </div>
+                        </article>
                     </Col>
+                    </div>
+                    <CallToAction/>
                 </Grid>
+
         )
     }
 
