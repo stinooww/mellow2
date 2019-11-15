bcd6f60a77da20a186c2d38c8a2d4c17ae3f41ab
stijnheynderickx <stijn.heynderickx@elision.eu>
2019-11-15 09:14:04 +0100

    cta

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
               hideModal={this.hideModal}
             />
           ) : null}
-          <CallToAction/>
+          <CallToAction activateOfferte={this.activateOfferte}/>
           <Footer />
         </div>
       </Router>
