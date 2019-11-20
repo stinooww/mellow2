f07d1a4cb873d7f070f72d947592e0c3f098d9ea
stijnheynderickx <stijn.heynderickx@elision.eu>
2019-11-20 11:29:53 +0100

    loading animation

--- frontend/src/pages/Portfolio/Portfolio.jsx
+++ frontend/src/pages/Portfolio/Portfolio.jsx
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { CircleArrow as ScrollUpButton } from 'react-scroll-up-button';
 import { Col, Grid, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import Spinner from "../../components/Spinner/Spinner";
 // https://www.youtube.com/watch?v=oQnojIyTXb8  voor axios
 var ES6Promise = require('es6-promise');
 ES6Promise.polyfill();
@@ -11,7 +12,8 @@ class Portfolio extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      portfolio: []
+      portfolio: [],
+      isLoading: false
     };
   }
 
@@ -25,13 +27,14 @@ class Portfolio extends Component {
       })
       .then(res => {
         this.setState({
+          isLoading: true,
           portfolio: res.data.data
         });
       });
   }
 
   render() {
-    const { portfolio } = this.state;
+    const {portfolio, isLoading} = this.state;
 
     return (
       <div className="PortfolioPage">
@@ -46,56 +49,60 @@ class Portfolio extends Component {
               <section className="section section-projects">
                 <div className="document-inset section-content">
                   <ul className="section-content__list">
-                    {portfolio &&
-                      portfolio.map(project => (
-                        <li key={project.id}>
-                          <div className="table">
-                            <div className="row">
-                              <div className="cell image-cell">
-                                <div className="image-cell-container">
-                                  <Link to={`portfolioitem/${project.id}`}>
-                                    <figure
-                                      className="image"
-                                      style={{
-                                        backgroundImage:
-                                          'url(' +
-                                          project.attributes.Thumbnail +
-                                          ')'
-                                      }}
-                                    >
-                                      <span className="aspect-ratio" />
-                                    </figure>
-                                  </Link>
-                                </div>
-                              </div>
-                              <div className="cell content-cell">
-                                <div className="content-cell-container">
-                                  <div>
-                                    <h2 className="title">
-                                      <Link
-                                        to={`/portfolioitem/${project.id}`}
-                                        key={project.id}
-                                      >
-                                        {project.attributes.clientName}
-                                      </Link>
-                                    </h2>
-                                    <h3 className="client">
-                                      <Link to={`/portfolioitem/${project.id}`}>
-                                        {project.attributes.title}
-                                      </Link>
-                                    </h3>
+                    {!isLoading && portfolio ? <Spinner/> : (
+                        portfolio.map(project => (
+                                <li key={project.id}>
+                                  <div className="table">
+                                    <div className="row">
+                                      <div className="cell image-cell">
+                                        <div className="image-cell-container">
+                                          <Link to={`portfolioitem/${project.id}`}>
+                                            <figure
+                                                className="image"
+                                                style={{
+                                                  backgroundImage:
+                                                      'url(' +
+                                                      project.attributes.Thumbnail +
+                                                      ')'
+                                                }}
+                                            >
+                                              <span className="aspect-ratio"/>
+                                            </figure>
+                                          </Link>
+                                        </div>
+                                      </div>
+                                      <div className="cell content-cell">
+                                        <div className="content-cell-container">
+                                          <div>
+                                            <h2 className="title">
+                                              <Link
+                                                  to={`/portfolioitem/${project.id}`}
+                                                  key={project.id}
+                                              >
+                                                {project.attributes.clientName}
+                                              </Link>
+                                            </h2>
+                                            <h3 className="client">
+                                              <Link to={`/portfolioitem/${project.id}`}>
+                                                {project.attributes.title}
+                                              </Link>
+                                            </h3>
+                                          </div>
+                                          <p className="view-project">
+                                            <Link to={`/portfolioitem/${project.id}`}>
+                                              Bekijk Project
+                                            </Link>
+                                          </p>
+                                        </div>
+                                      </div>
+                                    </div>
                                   </div>
-                                  <p className="view-project">
-                                    <Link to={`/portfolioitem/${project.id}`}>
-                                      Bekijk Project
-                                    </Link>
-                                  </p>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                        </li>
-                      ))}
+                                </li>
+                            )
+                        )
+                    )
+                    }
+
                   </ul>
                 </div>
               </section>
